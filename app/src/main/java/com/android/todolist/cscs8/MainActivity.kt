8b619ae279591fdb2cbff850e361c591a22c99cc
cs8 <42040439+cscs8@users.noreply.github.com>
2020-05-19 22:46:37 +0900

    add ResultActivity

--- app/src/main/java/com/android/todolist/cscs8/MainActivity.kt
+++ app/src/main/java/com/android/todolist/cscs8/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.android.todolist.cscs8
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,10 +18,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        // Buttonへの参照を取得する
+        val button = findViewById<Button>(R.id.button3)
+        // ボタンタップ時の処理を指定する
+        button.setOnClickListener {
+            // EditTextへの参照を取得する
+            val editText = findViewById<EditText>(R.id.textInputLayout)
+            // メッセージを取り出す
+            val message = editText.text.toString()
+
+            // Intentを作成する
+            val intent = Intent(this, ResultActivity::class.java)
+            // パラメータをセットする
+            intent.putExtra("message", message)
+            // 画面を遷移させる
+            startActivity(intent)
+
+//            // パラメータを取得する
+//            val message = intent.getStringExtra("message")
         }
+//        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
