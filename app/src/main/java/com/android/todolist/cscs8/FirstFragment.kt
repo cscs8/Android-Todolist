5a282053e0134fbfeba7e0a93503da7dafa26cc4
cs8 <42040439+cscs8@users.noreply.github.com>
2020-05-20 18:38:58 +0900

    improvement import format

--- app/src/main/java/com/android/todolist/cscs8/FirstFragment.kt
+++ app/src/main/java/com/android/todolist/cscs8/FirstFragment.kt
@@ -1,13 +1,10 @@
 package com.android.todolist.cscs8
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
