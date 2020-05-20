33faf97dd3c2f9de368bf6bf88d9913c382c062b
cs8 <42040439+cscs8@users.noreply.github.com>
2020-05-20 14:10:32 +0900

    improvement code

--- app/src/main/java/com/android/todolist/cscs8/adapter/TodoAdapter.kt
+++ app/src/main/java/com/android/todolist/cscs8/adapter/TodoAdapter.kt
@@ -3,6 +3,8 @@ package com.android.todolist.cscs8.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.android.todolist.cscs8.R
 import kotlinx.android.synthetic.main.recyclerview_item.view.*
@@ -13,9 +15,9 @@ import kotlinx.android.synthetic.main.recyclerview_item.view.*
 
 class TodoAdapter(private val dataset: ArrayList<String>) :
     RecyclerView.Adapter<TodoAdapter.MyViewHolder>() {
-    class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val sampleImg = view.sampleImg
-        val sampleTxt = view.sampleTxt
+    class MyViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val sampleImg: ImageView = view.sampleImg
+        val sampleTxt: TextView = view.sampleTxt
     }
 
 
@@ -23,12 +25,9 @@ class TodoAdapter(private val dataset: ArrayList<String>) :
         parent: ViewGroup,
         viewType: Int
     ): MyViewHolder {
-//        val textView = LayoutInflater.from(parent.context)
-//            .inflate(R.layout.recyclerview_item, parent, false) as TextView
-//        return MyViewHolder(textView)
-        val textView = LayoutInflater.from(parent.context)
+        val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.recyclerview_item, parent, false)
-        return MyViewHolder(textView)
+        return MyViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
